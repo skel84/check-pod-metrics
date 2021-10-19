28ebf0e160f20b71e2ad7ed0da4d2a2962f90a20
Francesco Cislaghi <francesco.cislaghi@bewatec.com>
2021-10-19 19:22:54 +0200

    fix to use influxdb output

--- bin/check-pod-metrics.sh
+++ bin/check-pod-metrics.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-kubectl top pod --all-namespaces --use-protocol-buffers | grep -v NAMESPACE | while read line; do echo $line | awk '{print $2".pod.cpu "substr($3, 1, length($3)-1)" "systime() "\n"$2".pod.mem "substr($4, 1, length($4)-2)" "systime() }'  ; done
+kubectl top pod --all-namespaces --use-protocol-buffers | grep -v NAMESPACE | while read line; do echo $line | awk '{print "pod_cpu,container_name="$2 " cpu_total="substr($3, 1, length($3)-1)" " systime()"\n""pod_mem,container_name="$2 " mem_total="substr($4, 1, length($4)-2)" " systime() }'  ; done
